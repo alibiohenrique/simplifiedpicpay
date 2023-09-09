416ea937054c860ddea3568cbb1b8771816e8fd4
alibiohenrique <alibio.henrique@gmail.com>
2023-09-08 23:47:48 -0300

    feat: adjustments

--- src/main/java/com/simplifiedpicpay/domain/transaction/Transaction.java
+++ src/main/java/com/simplifiedpicpay/domain/transaction/Transaction.java
@@ -2,10 +2,7 @@ package com.simplifiedpicpay.domain.transaction;
 
 import com.simplifiedpicpay.domain.user.User;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -15,12 +12,14 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 @EqualsAndHashCode(of = "id")
 
 //  These annotations are used indicate
 //  that instances of this class can be stored in a database table called "transactions."
 //  They also automatically generate getter and setter methods, a constructor with all args,
-//  and methods for comparing and hashing instances based on their "id" field.
+//  also a constructor with no args, and methods for comparing and hashing instances
+//  based on their "id" field.
 
 public class Transaction {
 
--- src/main/java/com/simplifiedpicpay/domain/user/User.java
+++ src/main/java/com/simplifiedpicpay/domain/user/User.java
@@ -1,10 +1,8 @@
 package com.simplifiedpicpay.domain.user;
 
+import com.simplifiedpicpay.dtos.UserDTO;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.math.BigDecimal;
 
@@ -13,9 +11,11 @@ import java.math.BigDecimal;
 @Getter // Getters
 @Setter // Setters
 @AllArgsConstructor // Create the constructor that has all the parameters
+@NoArgsConstructor // Create the no args constructor
 @EqualsAndHashCode(of = "id")  // Primary key of this table
 
 public class User {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
@@ -27,8 +27,19 @@ public class User {
     private String email; // The same logic apply for the email. Cannot have a duplicated email on the database
     private String password;
     private BigDecimal balance;
-
     @Enumerated(EnumType.STRING)
     private UserType userType; //This annotation basically tells our attribute represents a value or another(common or merchant)
+
+    public User(UserDTO data) {
+
+        this.firstName = data.firstName();
+        this.lastName = data.lastName();
+        this.balance = data.balance();
+        this.userType = data.userType();
+        this.password = data.password();
+        this.email = data.email();
+        this.document = data.document();
+
+    }
 
 }
