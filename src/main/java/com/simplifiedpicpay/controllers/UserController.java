93c17ff536fc9a43087e828669a1563c0cd576f9
alibiohenrique <alibio.henrique@gmail.com>
2023-09-08 23:51:53 -0300

    feat: create UserController

---
+++ src/main/java/com/simplifiedpicpay/controllers/UserController.java
@@ -0,0 +1,34 @@
+package com.simplifiedpicpay.controllers;
+
+import com.simplifiedpicpay.domain.user.User;
+import com.simplifiedpicpay.dtos.UserDTO;
+import com.simplifiedpicpay.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public ResponseEntity<User> createUser
+            (@RequestBody UserDTO user) { // This annotation is to tell that the body will come on the body of the requisition
+        User newUser = userService.createUser(user);
+        return new ResponseEntity<>(newUser, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+
+        List<User> users = this.userService.getAllUsers();
+        return new ResponseEntity<>(users, HttpStatus.OK);
+    }
+
+}
