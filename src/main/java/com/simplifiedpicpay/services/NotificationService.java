8d893b78d9e276de11b31d0c00ee354d881a4d97
alibiohenrique <alibio.henrique@gmail.com>
2023-09-08 23:53:54 -0300

    feat: create NotificationDTO

---
+++ src/main/java/com/simplifiedpicpay/services/NotificationService.java
@@ -0,0 +1,27 @@
+package com.simplifiedpicpay.services;
+
+import com.simplifiedpicpay.domain.user.User;
+import com.simplifiedpicpay.dtos.NotificationDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+public class NotificationService {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public void sendNotification(User user, String message) throws Exception {
+
+        String email = user.getEmail();
+        NotificationDTO noticationRequest = new NotificationDTO(email, message);
+        ResponseEntity<String> noticationResponse = restTemplate.postForEntity("o4d9z.mocklab.io/notify", noticationRequest, String.class);
+        if (!(noticationResponse.getStatusCode() == HttpStatus.OK)) {
+            System.out.println("Error sending notification");
+            throw new Exception("Notification Service is down");
+
+        }
+    }
+
+}
